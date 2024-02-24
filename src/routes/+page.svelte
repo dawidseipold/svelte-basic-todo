a3b7a0599a8f87d0871b12c973bdcd4979b0473b
dawidseipold <seipold.dawid@gmail.com>
2024-02-24 10:11:51 +0100

    style: change regular functions to arrow functions refactor: remove randomId() function and replace it with crypto.randomUUID() feat: add a functionality to save data todos in localStorage feat: add a loading state before displaying the todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,44 @@
 <script lang='ts'>
+	type Todo = {
+		id: string;
+		text: string;
+		done: boolean;
+	};
 	type Filter = 'All' | 'Undone' | 'Done';
+	type Loading = 'loading';
 
 	let filter = $state('All') as Filter;
 	const filterOptions = ['All', 'Undone', 'Done'];
 
-	const randomId = function(length: number) {
-  	return Number(Math.random().toString(36).substring(2, length + 2));
-	};
+	let todos = $state<Todo[] | Loading>('loading');
 
-  let todos = $state([
-		{
-			id: randomId(32),
-			text: 'Learn Svelte',
-			done: false
-		},
-		{
-			id: randomId(32),
-			text: 'Learn SvelteKit',
-			done: false
+	$effect(() => {
+		const storedTodos = localStorage.getItem('todos');
+
+		if (storedTodos) {
+			todos = JSON.parse(storedTodos);
+		} else {
+			todos = [];
 		}
-	]);
+	});
 
-  function addTodo(event: KeyboardEvent) {
+	$effect (() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	});
+	
+	
+  const addTodo = (event: KeyboardEvent) => {
 		if (event.key !== 'Enter') return;
 
 		const todoElement = event.target as HTMLInputElement;
 
-		const id = randomId(32)
+		const id = window.crypto.randomUUID();
 		const text = todoElement.value;
 		const done = false;
 
-		todos.push({ id, text, done });
+		if (todos !== 'loading') {
+			todos.push({ id, text, done });
+		}
 
     todoElement.value = '';
 	}
@@ -49,43 +57,47 @@
 		return []
 	};
 
-	const filteredTodos = $derived(setFilteredTodos())
-
 	const removeTodo = (event: Event) => {
 		const todoElement = event.target as HTMLElement
 		const todoId = todoElement.closest('.todo')?.getAttribute('data-id');
 
-		if (todoId) {
-			todos.splice(todos.findIndex((todo) => todo.id === Number(todoId)), 1);
+		if (todoId && todos !== 'loading') {
+			todos.splice(todos.findIndex((todo) => todo.id === todoId), 1);
 		}
 	};
+
+	const filteredTodos = $derived(setFilteredTodos())
 </script>
 
 <div class="wrapper">
 	<h1 class="title">Simple Todo Application</h1>
 
-	<select name="filter" id="filter" on:change={changeFilter}>
-		{#each filterOptions as option}
-			<option value={option}>{option}</option>
-		{/each}
-	</select>
-
-	<input class="form-input" type="text" placeholder="Add todo" on:keydown={addTodo} />
-
-	<div class="todos">
-		{#if filteredTodos.length === 0}
-			<p class="warning">No todos found</p>
-		{:else}
-			{#each filteredTodos as todo}
-				<div class="todo" data-id={todo.id}>
-					<input type="checkbox" bind:checked={todo.done} />
-					<input type="text" value={todo.text} />
-
-					<div class='delete-button' on:click={removeTodo}>X</div>
-				</div>
+	{#if todos === 'loading'}
+		<p class="warning">Loading...</p>
+	{:else}
+		<select name="filter" id="filter" on:change={changeFilter}>
+			{#each filterOptions as option}
+				<option value={option}>{option}</option>
 			{/each}
-		{/if}
-	</div>
+		</select>
+
+		<input class="form-input" type="text" placeholder="Add todo" on:keydown={addTodo} />
+
+		<div class="todos">
+			{#if filteredTodos.length === 0}
+				<p class="warning">No todos found</p>
+			{:else}
+				{#each filteredTodos as todo}
+					<div class="todo" data-id={todo.id}>
+						<input type="checkbox" bind:checked={todo.done} />
+						<input type="text" value={todo.text} />
+
+						<div class='delete-button' on:click={removeTodo}>X</div>
+					</div>
+				{/each}
+			{/if}
+		</div>
+	{/if}
 </div>
 
 <style>
